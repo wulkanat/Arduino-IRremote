d328da0ba12aba7a8e4c4aa3af2b0e87cd4d9cf6
Rafi Khan <zetoslab@gmail.com>
2017-04-23 00:25:48 -0600

    single test

--- test.sh
+++ test.sh
@@ -1,15 +1,15 @@
-PLATFORMIO_CI_SRC=examples/AiwaRCT501SendDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRrecord python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRrecvDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRrecvDumpV2 python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRrelay python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRremoteInfo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRsendDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRsendRawDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRtest python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/IRtest2 python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/JVCPanasonicSendDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/LGACSendDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/LegoPowerFunctionsSendDemo python2 -m platformio ci --lib="." --board=uno
-PLATFORMIO_CI_SRC=examples/LegoPowerFunctionsTests python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/AiwaRCT501SendDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRrecord python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRrecvDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRrecvDumpV2 python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRrelay python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRremoteInfo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRsendDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRsendRawDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRtest python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/IRtest2 python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/JVCPanasonicSendDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/LGACSendDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/LegoPowerFunctionsSendDemo python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
+PLATFORMIO_CI_SRC=examples/LegoPowerFunctionsTests python2 -m platformio ci --lib="." --board=uno --board=leonardo --board=pro16MHzatmega168 --board=btatmega328 
 
