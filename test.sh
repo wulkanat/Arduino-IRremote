df5a824303b0ccc4bc9c30887fb89632aeaf03b8
Rafi Khan <zetoslab@gmail.com>
2017-04-23 00:20:51 -0600

    travis ci

--- test.sh
+++ test.sh
@@ -6,3 +6,9 @@ PLATFORMIO_CI_SRC=examples/IRrelay python2 -m platformio ci --lib="." --board=un
 PLATFORMIO_CI_SRC=examples/IRremoteInfo python2 -m platformio ci --lib="." --board=uno
 PLATFORMIO_CI_SRC=examples/IRsendDemo python2 -m platformio ci --lib="." --board=uno
 PLATFORMIO_CI_SRC=examples/IRsendRawDemo python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/IRtest python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/IRtest2 python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/JVCPanasonicSendDemo python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/LGACSendDemo python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/LegoPowerFunctionsSendDemo python2 -m platformio ci --lib="." --board=uno
+PLATFORMIO_CI_SRC=examples/LegoPowerFunctionsTests python2 -m platformio ci --lib="." --board=uno
